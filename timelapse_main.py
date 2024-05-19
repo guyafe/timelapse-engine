f27775d1a2f083fba4810a625362bcaa6e861578
Guy Yafe <gyafe@tripactions.com>
2024-05-19 15:50:40 +0300

    Load Timelapse Info

--- timelapse_main.py
+++ timelapse_main.py
@@ -1,10 +1,26 @@
+import json
+import sys
+
 import ee
+
+from models.TimelapseInfo import TimelapseInfo
 
 
 def main():
     print("Starting timelapse engine...")
     ee.Authenticate()
-    ee.Initialize(project = 'timelapse-sample')
+    ee.Initialize(project='timelapse-sample')
+    timelapse_info = load_timelapse_info()
+
+
+def load_timelapse_info():
+    timelapse_info_path = sys.argv[1]
+    print(f"Loading timelapse_info from: {timelapse_info_path}")
+    timelapse_info_file = open(timelapse_info_path, "r")
+    timelapse_info_data = json.load(timelapse_info_file)
+    timelapse_info = TimelapseInfo(**timelapse_info_data)
+    print(f"Loaded timelapse_info_data: {timelapse_info}")
+    return timelapse_info
 
 
 if __name__ == "__main__":
